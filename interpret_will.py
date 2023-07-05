4891171f371aae6b7cdb18b181bfb5a21817425f
Clayton Morrison <claytonm@email.arizona.edu>
2023-07-04 17:03:52 -0700

    refactor

---
+++ interpret_will.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+""" interpret_will.py -- create a will model based on will text extractions """
+
+import json
+
+
+def load_extractions(filepath: str):
+    """
+    Load the JSON will text extractions from JSON file and return Extractions
+    :param filepath:
+    :return:
+    """
+    pass
+
+
+def create_will_model(extractions):
+    """
+
+    :param extractions:
+    :return:
+    """
+    pass
