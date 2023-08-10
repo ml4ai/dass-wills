e79dd804c25b8845a04ffc394ec764ff532c3cbc
Clayton Morrison <claytonm@email.arizona.edu>
2023-08-09 22:41:36 -0700

    first commit

---
+++ schemas/codegen_swagger_model.py
@@ -0,0 +1,13 @@
+
+
+# -----------------------------------------------------------------------------
+# Script to automate using swagger-codegen to generate the Python data model
+#   Two generation modes: TEXT_EXTRACTIONS, WILL_MODEL
+# Requires that swagger-codegen is installed
+#   On Mac with homebrew:  $ brew install swagger-codegen
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Constants
+# -----------------------------------------------------------------------------
