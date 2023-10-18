68821ff231ff878485a21c817f0837b2a23de01f
Clayton Morrison <claytonm@email.arizona.edu>
2023-10-17 17:00:18 -0700

    full draft

--- WILL_JSON/bequeath_01_specific_to_specific/will_text_extractions.py
+++ WILL_JSON/bequeath_01_specific_to_specific/will_text_extractions.py
@@ -1,13 +1,69 @@
 from schemas.model.te import (
+    te_entity,
+    te_event_bequeath,
     te_text_extractions
 )
 
 
 def make_instance():
 
-    extractions = te_text_extractions.TETextExtractions(
+    # Entities
 
-        entities=
+    entity_e1 = te_entity.TEEntity(
+        id="e1",
+        texts=["John Doe", "I", "my"],
+        type="Testator"
+    )
+
+    entity_e2 = te_entity.TEEntity(
+        id="e2",
+        texts=["Tom Doe"],
+        type="BeneficiaryNamed"
+    )
+
+    entity_e3 = te_entity.TEEntity(
+        id="e3",
+        texts=["Mary Hoover"],
+        type="BeneficiaryNamed"
+    )
+
+    entity_e4 = te_entity.TEEntity(
+        id="e4",
+        texts=["red car"],
+        type="Asset"
+    )
+
+    entity_e5 = te_entity.TEEntity(
+        id="e5",
+        texts=["vacuum cleaner"],
+        type="Asset"
+    )
+
+    # Events
+
+    event_v1 = te_event_bequeath.TEEventBequeath(
+        id="v1",
+        type="BequestAsset",
+        testator="e1",
+        asset="e4",
+        benefactor="e2"
+    )
+
+    event_v2 = te_event_bequeath.TEEventBequeath(
+        id="v2",
+        type="BequestAsset",
+        testator="e2",
+        asset="e5",
+        benefactor="e3"
+    )
+
+    # The Text Extractions (top-level structure)
+
+    extractions = te_text_extractions.TETextExtractions(
+        _date="2023-09-13",
+        text="I, John Doe, bequeath my red car to Tom Doe and my vacuum cleaner to Mary Hoover",
+        entities=[entity_e1, entity_e2, entity_e3, entity_e4, entity_e5],
+        events=[event_v1, event_v2]
     )
 
     return extractions
